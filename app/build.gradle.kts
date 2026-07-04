989776a567b1bfa6c97d9e504e56da2338bde0b3
DucAnhPrime🔥 <108879626+AnhLinhTheLions@users.noreply.github.com>
2026-07-04 09:37:47 +0700

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
 }
 
+// Lấy số build từ Jenkins, nếu không thấy thì mặc định là 1
 val buildNumber = System.getenv("BUILD_NUMBER")?.toIntOrNull() ?: 1
 
 android {
@@ -13,6 +14,7 @@ android {
 
     signingConfigs {
         create("release") {
+            // Dùng biến môi trường KEYSTORE_FILE, nếu không có thì trỏ file local
             storeFile = file(System.getenv("KEYSTORE_FILE") ?: "my_key.jks")
             storePassword = "12345678"
             keyAlias = "key0"
@@ -22,32 +24,38 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myapplication"
-
         minSdk = 24
         targetSdk = 34
 
-        // Tự động tăng theo số lần Jenkins build
         versionCode = buildNumber
-        versionName = "1.0.$buildNumber"
 
-        println("======================================")
-        println("VERSION CODE : $versionCode")
-        println("VERSION NAME : $versionName")
-        println("BUILD NUMBER : $buildNumber")
-        println("======================================")
+        // Tự động tạo versionName: 20260704-01
+        val currentDate = SimpleDateFormat("yyyyMMdd").format(Date())
+        val paddedBuildNumber = String.format("%02d", buildNumber)
+        versionName = "$currentDate-$paddedBuildNumber"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("release")
-
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+    }
+
+    // Tự động đổi tên file APK đầu ra
+    applicationVariants.all {
+        val variant = this
+        if (variant.buildType.name == "release") {
+            variant.outputs.map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+                .forEach { output ->
+                    output.outputFileName = "AlarmApp-${defaultConfig.versionName}.apk"
+                }
         }
     }
 
@@ -73,9 +81,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
 
     val roomVersion = "2.6.1"
-
     implementation("androidx.room:room-runtime:$roomVersion")
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
-
     implementation("com.google.android.material:material:1.12.0")
 }
